d66dcdc302c3aaa813cb3f9c2e98965367185cb3
BeneToko <131210264+BeneToko@users.noreply.github.com>
2023-08-30 21:01:25 +0100

    removed unnecessary information from code

--- src/App.js
+++ src/App.js
@@ -1,23 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+      <h1 className="App-header">
+        React Weather App
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          This website is coded by Benedicte Toko and is
+          <a
+            className="App-link"
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noreferrer"
+          >
+            Open-sourced
+          </a>
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      </h1>
     </div>
   );
 }
