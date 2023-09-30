d98f8dfcf9d6960f0f26912dee1adb4f4c0e760f
BeneToko <131210264+BeneToko@users.noreply.github.com>
2023-09-30 23:41:46 +0100

    amended icon

--- src/WeatherIcon.js
+++ src/WeatherIcon.js
@@ -1,7 +1,10 @@
 import React from "react";
 
-
 export default function WeatherIcon(response) {
   const icon = response.data.condition.icon;
-  return `http://shecodes-assets.s3.amazonaws.com/api/weather/icons/${icon}.png`;
+
+  function Icon() {
+    return `http://shecodes-assets.s3.amazonaws.com/api/weather/icons/${icon}.png`;
+  }
+  return <div>{Icon()}</div>;
 }
